5f83d67a16b07b7874e3d1e51bc8fab7d7cdcff3
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2022-05-11 14:46:09 -0400

    grid

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use rand::prelude::*;
 use rand_pcg::Pcg64;
 use wassily::prelude::*;
+use image::imageops::colorops::*;
 
 pub struct Model {
     noise_scale: f32,
     niose_factor: f32,
-    margin: f32,
+    margin: u32,
     img_index: usize,
     desat: f32,
     seed: u64,
@@ -16,7 +17,7 @@ impl Model {
     pub fn new(
         noise_scale: f32,
         niose_factor: f32,
-        margin: f32,
+        margin: u32,
         img_index: usize,
         desat: f32,
         seed: u64,
@@ -37,13 +38,13 @@ impl Model {
 impl Default for Model {
     fn default() -> Self {
         Self {
-            noise_scale: 0.033,
+            noise_scale: 0.01,
             niose_factor: 0.0,
-            margin: 250.0,
-            img_index: 58,
-            desat: 0.75,
-            seed: 1,
-            photo: "/Users/jeffreyrosenbluth/Rust/sketches/perspective/assets/balcony.png",
+            margin: 750,
+            img_index: 59,
+            desat: 0.5,
+            seed: 717,
+            photo: "/Users/jeffreyrosenbluth/Rust/sketches/perspective/assets/rivendell.png",
         }
     }
 }
@@ -58,40 +59,79 @@ fn main() {
     sketch.save();
 }
 
-fn view_fn(canvas: &mut Canvas, model: &Model) {
-    let mut palette = Palette::with_img(model.photo, Some(50));
-    palette.desaturate(model.desat);
-    canvas.fill((*LIGHTSKYBLUE).tint(0.80));
-    let ts = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.9, 1.0];
-    let us = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.9, 1.0];
-    let vs = [0.0, 0.1, 0.2, 0.3, 0.7, 0.8, 0.9, 1.0];
-    let left = pt(-model.margin, canvas.height() / 2);
-    let right = pt(canvas.width() + model.margin as u32, canvas.height() / 2);
-    let top = pt(canvas.width() / 2, 0);
-    let bottom = pt(canvas.width() / 2, canvas.height());
+fn sharpen(img: &DynamicImage, factor: f32) -> DynamicImage {
+    let mut kernel = [0.0; 9];
+    let n = -factor;
+    let p = factor * 4.0 + 1.0;
+    kernel[1] = n;
+    kernel[3] = n;
+    kernel[4] = p;
+    kernel[5] = n;
+    kernel[7] = n;
+    img.filter3x3(&kernel)
+}
 
-    let mut quads = perspective_quads(
-        left,
-        top,
-        bottom,
-        right,
-        &ts,
-        &us,
-        &vs,
-        model.noise_scale,
-        model.niose_factor,
+fn view_fn(canvas: &mut Canvas, model: &Model) {
+    let mut palette = Palette::with_img(model.photo, Some(100));
+    palette.desaturate(model.desat);
+    // canvas.fill((*LIGHTSKYBLUE).tint(0.80));
+    let mut img = open(model.photo).unwrap();
+    img = sharpen(&img, 10.0);
+    img = DynamicImage::ImageRgba8(grayscale_with_type_alpha(&img));
+    img = img.resize_to_fill(
+        canvas.width() as u32,
+        canvas.height() as u32,
+        imageops::FilterType::Lanczos3,
+    );
+    // img = sharpen(&img, 50.0);
+    let photo: Canvas = match img {
+        DynamicImage::ImageLuma8(_) => panic!("image format not supported"),
+        DynamicImage::ImageLumaA8(_) => panic!("image format not supported"),
+        DynamicImage::ImageRgb8(_) => img.as_rgb8().unwrap().into(),
+        DynamicImage::ImageRgba8(_) => img.as_rgba8().unwrap().into(),
+        DynamicImage::ImageLuma16(_) => panic!("image format not supported"),
+        DynamicImage::ImageLumaA16(_) => panic!("image format not supported"),
+        DynamicImage::ImageRgb16(_) => panic!("image format not supported"),
+        DynamicImage::ImageRgba16(_) => panic!("image format not supported"),
+        DynamicImage::ImageRgb32F(_) => panic!("image format not supported"),
+        DynamicImage::ImageRgba32F(_) => panic!("image format not supported"),
+        _ => todo!(),
+    };
+    let cv: Canvas = photo.into();
+    *canvas = cv;
+    let left_stops = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.9, 1.0];
+    let right_stops = [0.0, 0.1, 0.5, 0.65, 0.75, 0.9, 1.0];
+    let vert_stops = [0.0, 0.1, 0.2, 0.3, 0.7, 0.8, 0.9, 1.0];
+    let left_top = pt(0, canvas.height() / 2 - model.margin);
+    let left_bottom = pt(0, canvas.height() / 2 + model.margin);
+    let right_top = pt(canvas.width(), canvas.height() / 2 - model.margin);
+    let right_bottom = pt(canvas.width(), canvas.height() / 2 + model.margin);
+    let middle_top = pt(canvas.width() / 2 - 1000, 100);
+    let middle_bottom = pt(canvas.width() / 2 - 1000, canvas.height() - 400);
+
+    let mut quads = Grid::perspective_grid(
+        left_top,
+        left_bottom,
+        middle_top,
+        middle_bottom,
+        right_top,
+        right_bottom,
+        &vert_stops,
+        &left_stops,
+        &right_stops,
     );
 
+    quads.0.data = warp_points(&quads.0.data, model.noise_scale, model.niose_factor);
+
     let mut rng = Pcg64::seed_from_u64(model.seed);
-    for (k, quad) in quads.iter().enumerate() {
+    for (k, quad) in quads.quads().iter().enumerate() {
         let c = palette.rand_color();
-        let ps: Vec<Point> = quad.to_vec();
         let q = if k == model.img_index {
             100
         } else {
             rng.gen_range(0..100)
         };
-        let bbox = bounding_box(&ps, 100.0);
+        let bbox = bounding_box(quad, 100.0);
         let w = bbox.width() as u32;
         let h = bbox.height() as u32;
         let n = 0;
@@ -102,41 +142,41 @@ fn view_fn(canvas: &mut Canvas, model: &Model) {
                 let mut pattern_canvas = Canvas::new(canvas.width(), canvas.height());
                 let paint = pattern(&texture, &mut pattern_canvas, bbox);
                 ShapeBuilder::new()
-                    .points(&chaiken(ps.clone(), n, Trail::Closed))
+                    .points(&chaiken(quad, n, Trail::Closed))
                     .fill_color(c)
                     .stroke_color(c)
                     .build()
                     .draw(canvas);
                 ShapeBuilder::new()
-                    .points(&chaiken(ps, n, Trail::Closed))
+                    .points(&chaiken(quad, n, Trail::Closed))
                     .fill_paint(&paint)
                     .no_stroke()
                     .build()
                     .draw(canvas);
             }
             // Wood
-            10..=19 => {
+            10..=15 => {
                 let c1 = palette.rand_color();
                 let c2 = palette.rand_color();
                 let texture = wood(w, h, c1, c2, 0.5);
                 let mut pattern_canvas = Canvas::new(canvas.width(), canvas.height());
                 let paint = pattern(&texture, &mut pattern_canvas, bbox);
                 ShapeBuilder::new()
-                    .points(&chaiken(ps, n, Trail::Closed))
+                    .points(&chaiken(quad, n, Trail::Closed))
                     .fill_paint(&paint)
                     .no_stroke()
                     .build()
                     .draw(canvas);
             }
             // Marble
-            20..=29 => {
+            16..=29 => {
                 let c1 = palette.rand_color();
                 let c2 = palette.rand_color();
                 let texture = marble(w, h, c1, c2, 0.5);
                 let mut pattern_canvas = Canvas::new(canvas.width(), canvas.height());
                 let paint = pattern(&texture, &mut pattern_canvas, bbox);
                 ShapeBuilder::new()
-                    .points(&chaiken(ps, n, Trail::Closed))
+                    .points(&chaiken(quad, n, Trail::Closed))
                     .fill_paint(&paint)
                     .no_stroke()
                     .build()
@@ -150,7 +190,7 @@ fn view_fn(canvas: &mut Canvas, model: &Model) {
                 let mut pattern_canvas = Canvas::new(canvas.width(), canvas.height());
                 let paint = pattern(&texture, &mut pattern_canvas, bbox);
                 ShapeBuilder::new()
-                    .points(&chaiken(ps, n, Trail::Closed))
+                    .points(&chaiken(quad, n, Trail::Closed))
                     .fill_paint(&paint)
                     .no_stroke()
                     .build()
@@ -164,21 +204,35 @@ fn view_fn(canvas: &mut Canvas, model: &Model) {
                 let mut pattern_canvas = Canvas::new(canvas.width(), canvas.height());
                 let paint = pattern(&texture, &mut pattern_canvas, bbox);
                 ShapeBuilder::new()
-                    .points(&chaiken(ps, n, Trail::Closed))
+                    .points(&chaiken(quad, n, Trail::Closed))
                     .fill_paint(&paint)
                     .no_stroke()
                     .build()
                     .draw(canvas);
             }
             // Sand
-            50..=59 => {
+            50..=55 => {
                 let c1 = palette.rand_color();
                 let c2 = palette.rand_color();
                 let texture = sand(w, h, c1, c2, 10.0);
                 let mut pattern_canvas = Canvas::new(canvas.width(), canvas.height());
                 let paint = pattern(&texture, &mut pattern_canvas, bbox);
                 ShapeBuilder::new()
-                    .points(&chaiken(ps, n, Trail::Closed))
+                    .points(&chaiken(quad, n, Trail::Closed))
+                    .fill_paint(&paint)
+                    .no_stroke()
+                    .build()
+                    .draw(canvas);
+            }
+            // Granite
+            56..=69 => {
+                let c1 = palette.rand_color();
+                let c2 = palette.rand_color();
+                let texture = granite(w, h, c1, c2, 1.0);
+                let mut pattern_canvas = Canvas::new(canvas.width(), canvas.height());
+                let paint = pattern(&texture, &mut pattern_canvas, bbox);
+                ShapeBuilder::new()
+                    .points(&chaiken(quad, n, Trail::Closed))
                     .fill_paint(&paint)
                     .no_stroke()
                     .build()
@@ -189,7 +243,7 @@ fn view_fn(canvas: &mut Canvas, model: &Model) {
                 img = img.resize_to_fill(
                     bbox.width() as u32,
                     bbox.height() as u32,
-                    imageops::FilterType::CatmullRom,
+                    imageops::FilterType::Lanczos3,
                 );
                 let texture: Canvas = match img {
                     DynamicImage::ImageLuma8(_) => panic!("image format not supported"),
@@ -207,7 +261,7 @@ fn view_fn(canvas: &mut Canvas, model: &Model) {
                 let mut canvas_pat = Canvas::new(canvas.width(), canvas.height());
                 let paint = pattern(&texture, &mut canvas_pat, bbox);
                 ShapeBuilder::new()
-                    .points(&chaiken(ps, n, Trail::Closed))
+                    .points(&chaiken(quad, n, Trail::Closed))
                     .fill_paint(&paint)
                     .no_stroke()
                     .build()
@@ -215,7 +269,7 @@ fn view_fn(canvas: &mut Canvas, model: &Model) {
             }
             _ => {
                 ShapeBuilder::new()
-                    .points(&chaiken(ps.clone(), n, Trail::Closed))
+                    .points(&chaiken(quad, n, Trail::Closed))
                     .fill_color(c)
                     .stroke_color(c)
                     .build()
@@ -223,7 +277,7 @@ fn view_fn(canvas: &mut Canvas, model: &Model) {
                 let g = Grain::new(bbox.width() as u32, bbox.height() as u32, 0.15, 0.10);
                 let p = g.grain();
                 ShapeBuilder::new()
-                    .points(&chaiken(ps, n, Trail::Closed))
+                    .points(&chaiken(quad, n, Trail::Closed))
                     .fill_paint(&p)
                     .stroke_color(c)
                     .build()
